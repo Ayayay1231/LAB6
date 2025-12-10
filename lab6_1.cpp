b09546f359496b5f204ff586c6130d2327d2d95a
Ayayay1231 <champ25752@gmail.com>
2025-12-10 20:43:24 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -6,13 +6,13 @@ int main(){
     cout << "Enter an integer: ";
     cin >> num;
     while(num != 0){
-        cout << "Enter an integer: ";
-        cin >> num;
-        if(num %2 ==0 ){
+        if(num %2 == 0 and num != 0){
             even++;     
         }else{
             odd++;
         }
+        cout << "Enter an integer: ";
+        cin >> num;
     }
     
     cout << "#Even numbers = "<< even << endl;
